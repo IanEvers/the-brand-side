ad89d6486a5ade9f4d90e1e014acea4bff1d8f28
IanEvers <59263788+IanEvers@users.noreply.github.com>
2023-03-14 19:03:34 -0300

    si

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sanity({
     projectId: 'ma1lfvs2',
@@ -21,5 +24,5 @@ export default defineConfig({
     useCdn: true
   }), svelte()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
